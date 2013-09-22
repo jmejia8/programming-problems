dce312a653e74d67e7673a08b1079b35fdd5131a
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-09-21 22:48:23 -0500

    solucion parcial

---
+++ F - donde esta el cero/main.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+if __name__ == '__main__':
+    cadena = raw_input()
+    while cadena != '':
+        if cadena.startswith('0'):
+            print '0'
+        for i in xrange(1, len(cadena)): # Bucle sobre las cifras a considerar
+            test_numero = int(cadena[:i])
+            cuenta_cifras = i
+            cuenta_posiciones = 1
+            bueno = False
+            while True:
+                next = test_numero +1
+                n_cad = cadena[cuenta_cifras:cuenta_cifras+len(str(next))]
+                if n_cad.startswith('0'):
+                    print cuenta_posiciones
+                    bueno = True
+                    break
+                elif int(n_cad) != next:
+                    break
+                else:
+                    cuenta_cifras += len(n_cad)
+                    cuenta_posiciones += 1
+            if bueno:
+                break
+        cadena = raw_input()
